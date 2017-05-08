b365ab74f303eff0a94ed925b3744e4f4d101de7
Yevgen Berberyan <yberberyan@gmail.com>
2017-05-08 15:08:33 +0300

    initial commit

---
+++ src/main/java/me/berberyan/App.java
@@ -0,0 +1,13 @@
+package me.berberyan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
