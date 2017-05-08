2dcbee519cd77ca594000ec74301cbdb69e53f20
Yevgen Berberyan <yberberyan@gmail.com>
2017-05-08 17:59:02 +0300

    recursively find all files in dirs and subdirs with given extention

--- src/main/java/me/berberyan/App.java
+++ src/main/java/me/berberyan/App.java
@@ -1,13 +1,27 @@
 package me.berberyan;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+	private static final Logger LOGGER = LogManager.getLogger(App.class); 
+
+    public static void main( String[] args ) throws IOException {
+
+    		LOGGER.debug("main() invoked");
+    		
+    		List<Path> testFiles = Files.walk(Paths.get("."))
+    				.filter(p -> p.toString().endsWith(".java"))
+    				.collect(Collectors.toList());
+    		
+    		testFiles.stream().forEach(LOGGER::debug);
+
     }
 }
