09c964b68d2b083be03debbc7fd953d60af86439
Yevgen Berberyan <yberberyan@gmail.com>
2017-05-09 21:22:32 +0300

    integrational test set @Ignored, cause it need to be started separately of other tests

--- src/test/java/berberyan/AppTest.java
+++ src/test/java/berberyan/AppTest.java
@@ -7,12 +7,13 @@ import java.io.PrintStream;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class AppTest {
 
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	                          
+
 	@Before
 	public void setUp() {
 		System.setOut(new PrintStream(outContent)); 
@@ -20,13 +21,17 @@ public class AppTest {
 
 	@After
 	public void cleanUpStreams() {
-	    System.setOut(null);
+		System.setOut(null);
 	}
-	
+
+	//for running this test config.properties should be default
+	//modification of phone numbers in other files or changing config.properties will make it fail
+	//should run as single unit-test (not with bunch of others)
+	@Ignore
 	@Test
 	public void mainTest_noArgs() {
 		App.main(null);
 		String expected = "+7 (812) 123-4567\n+7 (555) 123-4567\n+7 (812) 111-4567\n+7 (812) 123-4567\n+7 (812) 130-4567\n+7 (812) 888-8888\n+7 (812) 987-6543\n+7 (555) 123-4567\n+7 (812) 111-4567\n+7 (812) 123-4567\n+7 (812) 130-4567\n+7 (812) 888-8888\n+7 (812) 987-6543\n"; 
-	    assertEquals(expected, outContent.toString());
+		assertEquals(expected, outContent.toString());
 	}
 }
